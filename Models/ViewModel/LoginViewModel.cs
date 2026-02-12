b8d55e0a76aa5fca20388c13a78c6491760ba31e
CharanPolimera <charan.polimera@ibm.com>
2026-02-12 17:21:26 +0530

    Login Code

---
+++ Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedAxis.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+
+        public string UserName { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
+using CAS.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<CasContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/CrediMgr/Login";
+        options.AccessDeniedPath = "/CrediMgr/AccessDenied";
+    });
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
